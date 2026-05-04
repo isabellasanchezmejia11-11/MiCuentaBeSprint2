1eb147c3a50630df18c96e412e48f85b2f3ba207
Isabella <isabellasanchezmejia11@gmail.com>
2026-05-04 14:51:36 -0500

    Fix category

--- MiCuentaBE-main/src/main/java/com/fabricaescuela/micuenta/infrastructure/persistence/repository/CategoryJpaRepository.java
+++ MiCuentaBE-main/src/main/java/com/fabricaescuela/micuenta/infrastructure/persistence/repository/CategoryJpaRepository.java
@@ -3,13 +3,16 @@ package com.fabricaescuela.micuenta.infrastructure.persistence.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.fabricaescuela.micuenta.domain.model.MovementType;
 import com.fabricaescuela.micuenta.infrastructure.persistence.entity.CategoryEntity;
 
 public interface CategoryJpaRepository extends JpaRepository<CategoryEntity, Long> {
 
-    List<CategoryEntity> findByUserIdOrUserIdIsNullAndType(Long userId, MovementType type);
+    @Query("SELECT c FROM CategoryEntity c WHERE (c.userId = :userId OR c.userId IS NULL) AND c.type = :type")
+    List<CategoryEntity> findByUserIdOrUserIdIsNullAndType(@Param("userId") Long userId, @Param("type") MovementType type);
 
     boolean existsByNameAndUserId(String name, Long userId);
 
